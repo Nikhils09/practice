6c78f5eb41ab4b3e94627d35aafadda52e41d305
Nikhils09 <sawantnikhil1998@gmail.com>
2019-11-10 03:36:31 +0530

    changes made

--- inf.sh
+++ inf.sh
@@ -6,7 +6,7 @@ echo -e "User details: $a\n"
 ip=`hostname -I`
 echo -e "ip is $ip\n"
 hd=`lshw -short`
-echo "Hardware info: $hd\n"
+echo "Hardware info of the system: $hd\n"
 }
 touch info.txt
 inf>info.txt
